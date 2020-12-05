25ca0fcc4da44a0012b219d324887277a20abcd3
King_Artas <m.s.kopyl.kharkov@gmail.com>
2020-12-05 04:31:12 +0200

    coming back!

--- FinalWork/Back-end/backend-project/src/company/controller/company.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('company')
-export class CompanyController {}
--- FinalWork/Back-end/backend-project/src/company/modules/company.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface CompanyI {
-    id: number;
-    name: string;
-    address: string;
-    servicerOfActivity: string;
-    numberOfEmployees: number;
-    description: string;
-    type: string;
-}
